d539030a6843267a9a1cbb1195817f30d2e42f6e
Daniel <danielmac789@gmail.com>
2025-03-25 14:55:31 -0300

    mudança de icone ao abrir navbar e add da sessão tecnologias

--- js/toggleButton.js
+++ js/toggleButton.js
@@ -1,12 +1,18 @@
 function openNavigation() {
 
     const menuToggle = document.querySelector('.cabecalho__menu__toggle');
+    const openIcon = document.querySelector('.menu-toggle i:nth-of-type(1)');
+    const closeIcon = document.querySelector('.menu-toggle i:nth-of-type(2)');
 
     const currentDisplay = window.getComputedStyle(menuToggle).display;
 
     if (currentDisplay === "none") {
         menuToggle.style.display = "flex";
+        openIcon.style.display = "none";
+        closeIcon.style.display = "block";
     } else {
         menuToggle.style.display = "none";
+        openIcon.style.display = "";
+        closeIcon.style.display = "none";
     }
 }
